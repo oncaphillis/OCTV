262e685015f3598a637d47b2fe60de34f24ad47f
Sebastian Kloska <oncaphillis@snafu.de>
2015-01-28 23:39:08 +0100

    Added cerntral Value holder class Environment

---
+++ src/net/oncaphillis/whatsontv/Environment.java
@@ -0,0 +1,7 @@
+package net.oncaphillis.whatsontv;
+
+public class Environment {
+	public static String VERSION   ="1.0";
+	public static String COPYRIGHT ="&copy; 2015 Sebastian Kloska (<a href='mailto:sebastian.kloska@snafu.de'>sebastian.kloska@snafu.de</a>)"; 
+
+}
