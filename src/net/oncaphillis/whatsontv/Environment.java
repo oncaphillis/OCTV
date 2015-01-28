bd77c4d68ec90fc3fb79fa941971a1678556cb0f
Sebastian Kloska <oncaphillis@snafu.de>
2015-01-28 23:57:16 +0100

    New Copyright

--- src/net/oncaphillis/whatsontv/Environment.java
+++ src/net/oncaphillis/whatsontv/Environment.java
@@ -2,6 +2,6 @@ package net.oncaphillis.whatsontv;
 
 public class Environment {
 	public static String VERSION   ="1.0";
-	public static String COPYRIGHT ="&copy; 2015 Sebastian Kloska (<a href='mailto:sebastian.kloska@snafu.de'>sebastian.kloska@snafu.de</a>)"; 
+	public static String COPYRIGHT ="&copy; 2015 Sebastian Kloska (<a href='http://www.oncaphillis.net/'>www.oncaphillis.net</a>; <a href='mailto:sebastian.kloska@snafu.de'>sebastian.kloska@snafu.de</a>)"; 
 
 }
