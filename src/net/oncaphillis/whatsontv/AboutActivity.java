2746d0bee60a9da72577c7f2d5f615b2d6303bf5
Sebastian Kloska <oncaphillis@snafu.de>
2015-01-28 23:57:42 +0100

    WebView handling in AboutActivity

--- src/net/oncaphillis/whatsontv/AboutActivity.java
+++ src/net/oncaphillis/whatsontv/AboutActivity.java
@@ -1,27 +1,52 @@
 package net.oncaphillis.whatsontv;
 
+import java.io.ByteArrayOutputStream;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Base64;
 import android.view.Window;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
 public class AboutActivity extends Activity {
 
-	private final String _cc = 			
-		"<html>"
-			 + "<body>"
-			 + "<div>"
-			 + "<div style='float:center'>"
-			 + "Icons made by <a href='http://www.icomoon.io' title='Icomoon'>Icomoon</a>,"
-			 + "<a href='http://catalinfertu.com' title='Catalin Fertu'>Catalin Fertu</a>,"
-			 + "<a href='http://linhpham.me/miu' title='Linh Pham'>Linh Pham</a>"
-			 + " from <a href='http://www.flaticon.com' title='Flaticon'>www.flaticon.com</a>"
-			 + " is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0'>CC BY 3.0</a>"
-			 + "</div>"
-			 + "</div>"
-			 + "</body>"
-			 + "</html>";
+	private final static String _prefix = 			
+    " <html>" +
+	"   <body>" +
+	"     <div style='font-height:1.5em; line-height:2em'>" +
+	"       <div style='margin:8px'> "+
+	"         <div style='float:left'> ";
+	
+	private final static String _middle= 
+	"         </div> "+
+	"         <h1>What's On TV "+Environment.VERSION+"</h1> "+ 
+	"         <p> "+
+	"          "+Environment.COPYRIGHT+"."+
+	"          Licensed under the <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GPLv2</a>. "+
+	"          Source available at <a href='https://github.com/oncaphillis/WhatsOnTV'>github.com/oncaphillis/WhatsOnTV</a>. Icons made by  "+
+	"           <a href='http://www.icomoon.io' title='Icomoon'>Icomoon</a>, "+
+	"           <a href='http://catalinfertu.com' title='Catalin Fertu'>Catalin Fertu</a>, "+
+	"           <a href='http://linhpham.me/miu' title='Linh Pham'>Linh Pham</a> "+
+	"          from <a href='http://www.flaticon.com' title='Flaticon'>www.flaticon.com</a> "+ 
+	"          is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0'>CC BY 3.0</a> "+
+	"         </p> "+ 
+	"       </div> "+
+	"       <hr> "+
+	"       <div style='float:left'>";
+
+	private final static String _postfix = 
+ 	"       </div>"+
+	"       <p style='vertical-align:top'> "+ 
+	"        This product uses the <a href='http://themoviedb.org'>TMDb</a> API but is not endorsed or certified by TMDb. "+ 
+	"       </p> "+
+	"     </div> "+
+	"   </body> "+
+	" </html> ";
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +57,44 @@ public class AboutActivity extends Activity {
 
 
 		WebView  webview    = ((WebView) findViewById(R.id.about_webview));
-	
-		webview.loadData(_cc,"text/html; charset=utf-8;", "utf-8");
 		
+		webview.loadData(_prefix+getBitmapHtml(R.drawable.ic_launcher,new Integer(128),null)+
+				_middle+getBitmapHtml(R.drawable.tmdb_logo,null,null)+
+				_postfix,"text/html; charset=utf-8;", "utf-8");
+		
+	}
+	
+	private String getBitmapHtml(Bitmap bm,Integer w,Integer h) {
+		if(bm==null)
+			return "";
+		
+	    // Convert bitmap to Base64 encoded image for web
+	    
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+	    bm.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+	    byte[] byteArray = byteArrayOutputStream.toByteArray();
+	    String imgageBase64 = Base64.encodeToString(byteArray, Base64.DEFAULT);
+	    
+	    String ws="";
+	    String hs="";
+	    
+	    if(w != null) {
+	    	ws = "width='"+w.toString()+"'";
+	    	hs = "height='auto'";
+	    }
+	    
+	    if(h != null) {
+	    	hs = "height='"+h.toString()+"'";
+	    	if(ws.equals("")) {
+	    		ws="width='auto'";
+	    	}
+	    }
+	    
+	    return "<img "+ws+" "+hs+" style='max-width:100%;padding-right:0.5cm;padding-bottom:0.5cm;float:left;' src='"+
+	    		"data:image/png;base64," + imgageBase64+"' />";
+	}
+
+	private String getBitmapHtml(int id,Integer w,Integer h)  {
+		return getBitmapHtml(((BitmapDrawable)getResources().getDrawable(id)).getBitmap(),w,h);
 	}
 }
