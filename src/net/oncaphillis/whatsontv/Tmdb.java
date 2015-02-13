72eed38ee78fd8b6bc21be1e2ee5f284e53b644b
Sebastian Kloska <oncaphillis@snafu.de>
2015-02-13 20:15:33 +0100

    Formating and stuff

--- src/net/oncaphillis/whatsontv/Tmdb.java
+++ src/net/oncaphillis/whatsontv/Tmdb.java
@@ -4,19 +4,76 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.concurrent.Semaphore;
 
+import net.oncaphillis.whatsontv.Tmdb.SeasonKey;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.View;
 import android.widget.ProgressBar;
 import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.TmdbTV.TvMethod;
+import info.movito.themoviedbapi.TmdbTvEpisodes.EpisodeMethod;
+import info.movito.themoviedbapi.TmdbTvSeasons.SeasonMethod;
 import info.movito.themoviedbapi.model.config.Timezone;
+import info.movito.themoviedbapi.model.tv.TvEpisode;
+import info.movito.themoviedbapi.model.tv.TvSeason;
 import info.movito.themoviedbapi.model.tv.TvSeries;
+
+abstract class CacheMap<K,V> {
+	
+	private int _max = 5000;
+	
+	class Node {
+		Node(V s) {
+			value = s;
+		}
+		public int count;
+		public V value;  
+	}
+
+	private Hashtable<K,Node> _map = new Hashtable<K,Node>();	
+	
+	CacheMap(int max) {
+		_max = max;
+	}
+	
+	public void put(K id,V value) {
+		if(_map.get(id)!=null) {
+			_map.get(id).value = value;
+		} else {
+			_map.put(id,new Node(value));
+		}
+	}
+	
+	public V get(K key) {
+		Node n;
+		
+		if((n = _map.get(key)) == null) {
+			V v = load(key);
+			if(v!=null) {
+				_map.put(key,n=new Node(v));
+			}
+		}
+		
+		if(n!=null) {
+			n.count++;
+			return n.value;
+		}
+		
+		return null;
+	}
+	
+	public int size() {
+		return _map.size();
+	}
+	abstract V load(K key);
+};
 
 /**
  * A cash for Bitmap objects we retrieve from the Web. It holds 
@@ -26,6 +83,7 @@ import info.movito.themoviedbapi.model.tv.TvSeries;
  * @author kloska
  *
  */
+
 class BitmapHash {
 	
 	private class Node {
@@ -106,14 +164,65 @@ class BitmapHash {
 		}
 		return null;
 	}
+
+	public int size() {
+		Iterator<Integer> i = _bmHash.keySet().iterator();
+		int s = 0;
+		while(i.hasNext()) {
+			s+=_bmHash.get(i.next()).size();
+		}
+		return s;
+	}
 }
 
 public class Tmdb {
-	private static Tmdb      _one  = null;
-	private static String    _key  = null;
-	private TmdbApi          _api  = null;
-	private BitmapHash       _hash = new BitmapHash();
-	private List<Timezone> _timezones= null;
+	private static Tmdb      _one     = null;
+	private static String    _key     = null;
+	private TmdbApi          _api     = null;
+	private BitmapHash       _hash    = new BitmapHash();
+	private List<Timezone> _timezones = null;
+
+	class SeasonKey {
+		SeasonKey(int series, int season) {
+			this.series = series;
+			this.season = season;
+		}
+		int	series;
+		int season;
+	};
+	
+	class EpisodeKey {
+		EpisodeKey(int series, int season,int episode) {
+			this.series  = series;
+			this.season  = season;
+			this.episode = episode;
+		}
+		int	series;
+		int season;
+		int episode;
+	};
+	
+	private CacheMap<Integer,TvSeries> _series  = new CacheMap<Integer,TvSeries>(5000) {
+		@Override
+		TvSeries load(Integer key) {
+			return api().getTvSeries().getSeries(key, getLanguage(), TvMethod.external_ids,TvMethod.images,TvMethod.credits);
+		}
+	};
+
+	private CacheMap<SeasonKey,TvSeason>   _seasons  = new CacheMap<SeasonKey,TvSeason>(5000) {
+		@Override
+		TvSeason load(SeasonKey key) {
+			return api().getTvSeasons().getSeason(key.series, key.season, getLanguage(),
+					SeasonMethod.external_ids, SeasonMethod.credits);
+		}
+	};
+	private CacheMap<EpisodeKey,TvEpisode> _episodes = new CacheMap<EpisodeKey,TvEpisode>(5000) {
+		@Override
+		TvEpisode load(EpisodeKey key) {
+			return api().getTvEpisodes().getEpisode(key.series,key.season,key.episode, 
+					getLanguage(), EpisodeMethod.credits,EpisodeMethod.external_ids,EpisodeMethod.images);
+		}
+	};
 
 	private Tmdb() {
 		final Semaphore mutex = new Semaphore(0);
@@ -153,8 +262,8 @@ public class Tmdb {
 			
 			if(path!=null && (bm=_hash.get(size,path))==null) {
 				try {
+					final ProgressBar p = pb;
 					if(act!=null && pb!=null) {
-						final ProgressBar p = pb;
 						act.runOnUiThread(new Runnable(){
 							@Override
 							public void run() {
@@ -167,6 +276,14 @@ public class Tmdb {
 					bm = BitmapFactory.decodeStream(is);
 					if(bm != null) {
 						_hash.put(bm,size,path);
+					}
+					if(p!=null) {
+						act.runOnUiThread(new Runnable(){
+							@Override
+							public void run() {
+								p.setVisibility(View.INVISIBLE);
+							}
+						});
 					}
 				} catch(Exception ex) {
 				}
@@ -191,10 +308,17 @@ public class Tmdb {
 	}
 
 	public TvSeries loadSeries(int id) {
-		
-		return null;
+		return _series.get(id);
 	}
 	
+	public TvSeason loadSeason(int series,int season) {
+		return _seasons.get(new SeasonKey(series,season));
+	}
+
+	public TvEpisode loadEpisode(int series,int season,int episode) {
+		return _episodes.get(new EpisodeKey(series,season,episode));
+	}
+
 	/** returns the TmdbApi object we are working with. Might be null
 	 * if we currently don't have a connection.
 	 *  
@@ -216,7 +340,31 @@ public class Tmdb {
 		return _timezones;
 	}
 
+	
+	public static CacheMap getSeriesCache() {
+		return get()._series;
+	}
+	
+	public static CacheMap getSeasonsCache() {
+		return get()._seasons;
+	}
+	
+	public static CacheMap getEpisodeCache() {
+		return get()._episodes; 
+	}
+	
+	public static BitmapHash getBitmapCache() {
+		return get()._hash;
+	}
 	public static String getLanguage() {
 		return "en";
+	}
+
+	public static boolean isDebug() {
+		return true;
+	}
+
+	public static Timezone getTimezone() {
+		return null;
 	}
 }
