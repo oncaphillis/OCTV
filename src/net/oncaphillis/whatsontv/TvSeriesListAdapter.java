458bae25612339fdd31cd99863d8ea655fe5cbfb
Sebastian Kloska <oncaphillis@snafu.de>
2015-02-13 20:13:36 +0100

    Use background job for Series info (aka Network...)

--- src/net/oncaphillis/whatsontv/TvSeriesListAdapter.java
+++ src/net/oncaphillis/whatsontv/TvSeriesListAdapter.java
@@ -1,6 +1,7 @@
 package net.oncaphillis.whatsontv;
 
 import info.movito.themoviedbapi.model.Genre;
+import info.movito.themoviedbapi.model.tv.Network;
 import info.movito.themoviedbapi.model.tv.TvSeries;
 
 import java.util.HashMap;
@@ -8,7 +9,6 @@ import java.util.Iterator;
 import java.util.List;
 
 import net.oncaphillis.whatsontv.R;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
@@ -49,39 +49,48 @@ class TvSeriesListAdapter extends ArrayAdapter<TvSeries> {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		
-		View v = convertView;
+		View the_view = convertView;
 		
-		if(v == null) {
+		if(the_view == null) {
 	        LayoutInflater vi;
 	        vi = LayoutInflater.from(getContext());
-	        v  = vi.inflate(R.layout.series_list_entry, null);						
+	        the_view  = vi.inflate(R.layout.series_list_entry, null);						
 		}
 		
-		TvSeries s = getItem(position);
+		TvSeries the_series = getItem(position);
 		
 		// v.setBackgroundColor(R.id.action_bar);
 		
-	    if (s != null && Tmdb.get().api()!=null) {
-	    	TextView   tt0 = (TextView)     v.findViewById(R.id.series_title);
-	    	ImageView   ii = (ImageView)    v.findViewById(R.id.series_list_image);
-	    	TextView   tt_vote = (TextView)     v.findViewById(R.id.series_rating);
-	    	ProgressBar pb = (ProgressBar)  v.findViewById(R.id.series_wait_bar);
+	    if (the_series != null && Tmdb.get().api()!=null) {
+	    	TextView   tt0 = (TextView)     the_view.findViewById(R.id.series_title);
+	    	TextView   tt1 = (TextView)     the_view.findViewById(R.id.series_info);
+	    	
+	    	ImageView       ii = (ImageView)    the_view.findViewById(R.id.series_list_image);
+	    	TextView   tt_vote = (TextView) the_view.findViewById(R.id.series_rating);
+	    	ProgressBar     pb = (ProgressBar)  the_view.findViewById(R.id.series_wait_bar);
 
 	    	pb.setVisibility(View.INVISIBLE);
 	    	
-	    	if(s.getPosterPath() != null) {
-		    	if(ii.getTag()==null || !ii.getTag().toString().equals(s.getPosterPath())) { 
+	    	if(the_series.getPosterPath() != null) {
+		    	if(ii.getTag()==null || !ii.getTag().toString().equals(the_series.getPosterPath())) { 
 		    		ii.setImageBitmap(_defBitmap);
 		    	}
-	    		ii.setTag(s.getPosterPath());
+	    		ii.setTag(the_series.getPosterPath());
 	    		new BitmapDownloaderTask(ii,_activity,pb,_defBitmap,null).execute();
     		} else {
 	    		ii.setTag(null);
     			ii.setImageBitmap(_defBitmap);
     		}
-    		tt0.setText( s.getName() );
-	    	tt_vote.setText( String.format("%.1f", s.getVoteAverage())+"/"+Integer.toString(s.getVoteCount()));
+
+	    	
+	    	tt0.setText( the_series.getName() );
+
+	    	if(tt1.getTag() == null || !(tt1.getTag() instanceof Integer) || !((Integer)tt1.getTag()).equals(the_series.getId()) ) {
+    			tt1.setTag(new Integer(the_series.getId()));
+    			new SeriesInfoDownLoaderTask(tt1,_activity,pb,_defBitmap,null).execute();
+    		} 
+	    	tt_vote.setText( String.format("%.1f", the_series.getVoteAverage())+"/"+Integer.toString(the_series.getVoteCount()));
 	    }
-	    return v;
+	    return the_view;
 	}
 }
