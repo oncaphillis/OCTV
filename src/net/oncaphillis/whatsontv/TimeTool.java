00e15ef6c09434bcb678a8f5b4cbfa7f63fb61f7
Sebastian Kloska <oncaphillis@snafu.de>
2015-02-25 02:52:50 +0100

    new class TimeTool with service methods for date calculation

---
+++ src/net/oncaphillis/whatsontv/TimeTool.java
@@ -0,0 +1,68 @@
+package net.oncaphillis.whatsontv;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class TimeTool {
+	private static DateFormat _toFormater   = DateFormat.getDateTimeInstance(DateFormat.DEFAULT,DateFormat.SHORT,Locale.getDefault());
+	private static DateFormat _fromFormater = new SimpleDateFormat("yyyy-MM-dd");
+
+	public static Calendar toTimeZone(Calendar ci,String tz) {
+
+		TimeZone toTimeZone = TimeZone.getTimeZone(tz);
+		Calendar c = (Calendar) ci.clone();
+		
+		String s = c.getTimeZone().getID();
+		
+		// Transform to UTC + DST
+		
+		/* c.add(Calendar.MILLISECOND, c.getTimeZone().getRawOffset());
+
+		if (c.getTimeZone().inDaylightTime(c.getTime())) {
+            c.add(Calendar.MILLISECOND, +c.getTimeZone().getDSTSavings() * -1);
+        }
+        */
+		
+        c.add(Calendar.MILLISECOND, toTimeZone.getRawOffset());
+        
+        if (toTimeZone.inDaylightTime(c.getTime())) {
+            c.add(Calendar.MILLISECOND, +toTimeZone.getDSTSavings() * -1);
+        }
+        
+        return c;
+	}
+	
+	public static Calendar getNow() {
+		Calendar nw = Calendar.getInstance();
+		return nw;
+	}
+
+	public static Calendar getToday() {
+		Calendar nw = getNow();
+		nw.set(Calendar.HOUR_OF_DAY, 0);
+		nw.set(Calendar.MINUTE, 0);
+		nw.set(Calendar.SECOND, 0);
+		nw.set(Calendar.MILLISECOND, 0);
+		return nw;
+	}
+	
+	public static Calendar fromString(String str) {
+		Calendar c = Calendar.getInstance();
+		Date d = null;
+		try {
+			d = _fromFormater.parse(str);
+		} catch (ParseException e) {
+		}
+		c.setTime(d);
+		return c;
+	}
+	
+	public static String toString(Calendar cw) {
+		return _toFormater.format(cw.getTime());
+	}
+}
