73a1486c561d0d9d822dc4aaa6c17b8cc9896302
Sebastian Kloska <oncaphillis@snafu.de>
2015-03-06 16:00:33 +0100

    More flesh to the Episode pager

--- src/net/oncaphillis/whatsontv/EpisodeCollectionPagerAdapter.java
+++ src/net/oncaphillis/whatsontv/EpisodeCollectionPagerAdapter.java
@@ -1,19 +1,27 @@
 package net.oncaphillis.whatsontv;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 public class EpisodeCollectionPagerAdapter extends FragmentStatePagerAdapter {
 	
-	public EpisodeCollectionPagerAdapter(FragmentManager fm) {
+	public EpisodeCollectionPagerAdapter(FragmentManager fm, EpisodePagerActivity episodePagerActivity) {
 		super(fm);
 	}
 
+
 	@Override
 	public Fragment getItem(int arg0) {
-		return null;
-	}
+        Fragment fragment = new EpisodeObjectFragment();
+        Bundle args       = new Bundle();
+        /*args.putInt(SeriesObjectFragment.ARG_IX, i);
+        args.putIntArray("ids", _ids);
+        args.putStringArray("names", _names);*/
+        fragment.setArguments(args);
+        return fragment;
+    }
 
 	@Override
 	public int getCount() {
--- src/net/oncaphillis/whatsontv/EpisodePagerActivity.java
+++ src/net/oncaphillis/whatsontv/EpisodePagerActivity.java
@@ -3,33 +3,54 @@ package net.oncaphillis.whatsontv;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class EpisodePagerActivity extends FragmentActivity {
 
+	private EpisodeCollectionPagerAdapter _episodePagerAdapter = null;
+	private ViewPager _viewPager;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_episode_pager);
+		_episodePagerAdapter  = new EpisodeCollectionPagerAdapter(getSupportFragmentManager(),this);
+		
+		_viewPager = (ViewPager) findViewById(R.id.series_page_layout);
+        _viewPager.setAdapter(_episodePagerAdapter);
+        _viewPager.setCurrentItem(0);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.episode_pager, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	@Override
+	protected void	onPause() {
+		/*if(SearchThread!=null)
+			SearchThread.lock();
+		*/
+		super.onPause();
+	}
+	
+	@Override
+	protected void	onResume() {
+		/*if(SearchThread!=null)
+			SearchThread.release();
+		*/
+		super.onResume();
+	}
+
 }
