a3a5ccfaea03eb266c434cab9043d90c5ba612bc
Sebastian Kloska <oncaphillis@snafu.de>
2015-03-16 17:58:36 +0100

    PropScaleImageView supporting vertical or horizontal fit

--- src/net/oncaphillis/whatsontv/PropScaleImageView.java
+++ src/net/oncaphillis/whatsontv/PropScaleImageView.java
@@ -7,6 +7,9 @@ import android.widget.ImageView;
 
 public class PropScaleImageView extends ImageView {
 
+	private boolean _fitV = false;
+	private boolean _fitH   = true;
+	
     public PropScaleImageView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
     }
@@ -14,10 +17,14 @@ public class PropScaleImageView extends ImageView {
     @Override
     protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
         final Drawable d = this.getDrawable();
-
-        if (d != null) {
+ 
+        if (d != null && _fitH) {
         	final int width = MeasureSpec.getSize(widthMeasureSpec);
         	final int height = (int) Math.ceil(width * (float) d.getIntrinsicHeight() / d.getIntrinsicWidth());
+            this.setMeasuredDimension(width, height);
+        } else if (d != null && _fitV) {
+        	final int height = MeasureSpec.getSize(heightMeasureSpec);
+        	final int width  = (int) Math.ceil(height * (float) d.getIntrinsicWidth() / d.getIntrinsicHeight());
             this.setMeasuredDimension(width, height);
         } else {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
