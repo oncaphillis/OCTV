e464fb1db7f341f14c6fd53ffa93c8d9b6065a9c
Sebastian Kloska <oncaphillis@snafu.de>
2015-03-19 12:12:19 +0100

    Simplified Exception handling in TraktReaderThread

--- src/net/oncaphillis/whatsontv/TraktReaderThread.java
+++ src/net/oncaphillis/whatsontv/TraktReaderThread.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.WeakHashMap;
 import java.util.concurrent.Semaphore;
 
 import com.uwetrottmann.trakt.v2.entities.Episode;
@@ -13,65 +14,106 @@ import com.uwetrottmann.trakt.v2.enums.IdType;
 
 public class TraktReaderThread extends Thread {
 	private Semaphore _lock          = new Semaphore(0);
-	private int _N = 0;
+
 	private Map<Integer,Episode> _map = new HashMap<Integer,Episode>(); 
 	private LinkedList<Integer> _list = new LinkedList<Integer>();
+	private WeakHashMap<Runnable,Void> _listeners = new WeakHashMap<Runnable,Void>();
+	
 	
 	@Override
 	public void run()  {
+
 		while(true) {
+		
 			try {
 				_lock.acquire();
-				int n = 0;
-				synchronized(this) {
-					n = _list.peekFirst();
-				}
-				try {
-					List<SearchResult> l;
-					l = Tmdb.get().trakt().search().idLookup(IdType.TMDB,Integer.toString(n), 1, null);
-					
-					if(l != null) {
-						for(SearchResult r : l) {
-							if(r.type.equals("episode") ) {
-								Episode eps = Tmdb.get().trakt().episodes().summary(Integer.toString(r.show.ids.trakt),r.episode.season, 
-										r.episode.number, Extended.FULL);
-								if(eps.first_aired!=null) {
-									synchronized(this) {
-										_map.put(n,eps);
-									}
+			} catch (InterruptedException e1) {
+			}
+			
+			int n = 0;
+
+			synchronized(this) {
+				n = _list.peekFirst();
+			}
+
+			try {
+				Episode eps = null;
+				List<SearchResult> l;
+
+				l = Tmdb.get().trakt().search().idLookup(IdType.TMDB,Integer.toString(n), 1, null);					
+
+				if(l != null) {
+
+					for(SearchResult r : l) {
+						if(r.type.equals("episode") ) {
+
+							eps = Tmdb.get().trakt().episodes().summary(Integer.toString(r.show.ids.trakt),r.episode.season, 
+									r.episode.number, Extended.FULL);
+
+							if(eps.first_aired!=null) {
+								synchronized(this) {
+									_map.put(n,eps);
+
 								}
 							}
 						}
 					}
-				} catch(Throwable t) {
-					t.printStackTrace();
 				}
-				
+
 				synchronized(this) {
 					_list.removeFirst();
+					if(_list.isEmpty())
+						inform();
 				}
-				
-				Thread.sleep(100);
-				_N++;
-			} catch (InterruptedException e) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+				}
+			} catch(Throwable t) {
+				synchronized(this) {
+					_list.removeFirst();
+					_list.addLast(n);
+					_lock.release();
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+				}
 			}
-			synchronized(this) {
-				_N++;
-			}
+		}
+	}
+	
+	public void register(Runnable r) {
+		synchronized(this) {
+			_listeners.put(r, null);
 		}
 	}
 	
 	public Episode get(int id) {
 		synchronized(this) {
-			add(id);
+			if( !_map.containsKey(id) )  {
+				add(id);
+			}
 			return _map.get(id);
 		}
 	}
 
+	private void inform() {
+		Runnable o;
+		synchronized(this) {
+			for(Runnable r : _listeners.keySet()) {
+				try {
+					r.run();
+				} catch(Throwable t) {
+					o= r;
+				}
+			}
+		}
+	}
 	private void add(int id) {
 		synchronized(this) {
 			if(!_map.containsKey( id )) {
-				_map.put( id ,null);
+				_map.put( id ,null );
 				_list.add(id);
 				_lock.release();
 			}
