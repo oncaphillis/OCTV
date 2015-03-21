70d3f1868695371bdce16de629eeb9306fb61b26
Sebastian Kloska <oncaphillis@snafu.de>
2015-03-21 09:32:22 +0100

    Typo

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 A TV Program Guide based on <a href="http://themoviedb.org">Tmdb</a> and <a href="http://trakt.tv/">Trakt</a>
  
-* One may browse through currently playing or popular TV shows.
+* Browse through currently playing or popular TV shows.
 
 * Search for shows by name and contents.
 
-* Flip thoufg seasons and episodes.
+* Flip through seasons and episodes.
 
 See http://www.oncaphillis.net/android.html for details 
 and download.
--- src/net/oncaphillis/whatsontv/TmdbKey.java
+++ src/net/oncaphillis/whatsontv/TmdbKey.java
@@ -9,6 +9,10 @@ package net.oncaphillis.whatsontv;
  */
 
 public class TmdbKey {
-	static final String APIKEY  = "Insert TMDB API Key here. See https://www.themoviedb.org/faq/api";
-	static final String TRAKTID = "Insert TRAKT API Id here. http://docs.trakt.apiary.io";
+	//static final String APIKEY = "Insert TMDB API Key here. See https://www.themoviedb.org/faq/api";
+	static final String APIKEY        = "b1f45d094cb0c142dba7442cb5dd7341";
+	
+	static final String TRAKTID       = "fe496b1bdee9b8b5176fc00219c121f93c00bef079972ceddee5d9f2935d1141";
+	// static final String TRAKTSECRET   = "66437604c739b59cd23900622f42d790f066d585e0aa17e7a3b3f9a88b570d14";
+	// static final String TRAKTTOK      = "638580e8c0d4b82c9ed0b4a04b2aa2ab7fe17d1add55df50e291b9a8c3566a28";
 }
