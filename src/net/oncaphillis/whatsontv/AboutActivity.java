e6eb5a69ccd908540672f48d48fe16332e0dcfb9
Sebastian Kloska <oncaphillis@snafu.de>
2015-03-27 02:05:39 +0100

    Fixes #30. Beautified "About..." with Trakt.tv logo and reference

--- src/net/oncaphillis/whatsontv/AboutActivity.java
+++ src/net/oncaphillis/whatsontv/AboutActivity.java
@@ -16,52 +16,49 @@ import android.widget.ImageView;
 
 public class AboutActivity extends Activity {
 
-	private final static String _prefix = 			
-    " <html>" +
-	"  <body>" +
-	"   <div style='font-height:1.5em; line-height:2em'>" +
-	"    <div style='margin:8px'> "+
-	"     <div style='float:left'> ";
-	
-	private final static String _middle= 
-	"     </div> "+
-	"     <h1>"+Environment.NAME+" "+Environment.VERSION+"</h1> "+ 
-	"     <p> "+
-	"      "+Environment.COPYRIGHT+"."+
-	"      Licensed under the <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GPLv2</a>. "+
-	"      Source available at <a href='https://github.com/oncaphillis/WhatsOnTV'>github.com/oncaphillis/WhatsOnTV</a>. "+
-	"      <i>'themoviedbapi'</i>&copy; 2014-15 by Holger Brandl. "+
-	"      <i>'trakt-java'</i>&copy; 2013-2015 by Uwe Trottmann."+
-	"      Icons made by  "+
-	"      <a href='http://www.icomoon.io' title='Icomoon'>Icomoon</a>, "+
-	"      <a href='http://catalinfertu.com' title='Catalin Fertu'>Catalin Fertu</a>, "+
-	"      <a href='http://linhpham.me/miu' title='Linh Pham'>Linh Pham</a> "+
-	"      from <a href='http://www.flaticon.com' title='Flaticon'>www.flaticon.com</a> "+ 
-	"      is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0'>CC BY 3.0</a> "+
-	"     </p> "+ 
-	"    <hr> "+
-	"    <p/>"+
-	"    <div>"+
-	"     <b>Powered by: <a href='http://themoviedb.org'>TMDb</a> and <a href='http://trakt.tv'>Trakt.tv</a></b>"+
-	"    </div>"+
-	"    <div>";
-	
-	private final static String _tmdb_postfix = 
-	"    </div>"+
-	"    <p/>"+
-	"    <p>"+ 
-	"     This product uses the TMDB API but is not endorsed or certified by TMDb. "+ 
-	"    </p>"+
-	"    <div>";
-	
-	private final static String _trakt_postfix = 
-	"    </div>"+
-	"    <hr>";
-	private final static String _terminal_postfix = 
-	"    </div> "+
-	"   </div> "+
-	"  </body> "+
-	" </html> ";
+	private final static String _html = 			
+			"<!DOCTYPE HTML PUBLIC -//W3C//DTD HTML 4.01 Transitional//EN>"+
+					" <html> "+
+					"  <body> "+
+					"    <div style='font-height:1.5em; line-height:2em; font-family:helvetica'> "+
+					"      <div style='margin:8px'> "+
+					"       <div style='float:left'> "+
+					"         %s"+
+					"       </div> "+
+					"       <h1>%s %s</h1>  "+
+					"       <p> "+
+					"         %s"+
+					"         Licensed under the <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GPLv2</a>. "+
+					"         Source available at <a href='https://github.com/oncaphillis/WhatsOnTV'>github.com/oncaphillis/WhatsOnTV</a>. "+
+					"         <i>'themoviedbapi'</i>&copy; 2014-15 by Holger Brandl. "+
+					"         <i>'trakt-java'</i>&copy; 2013-2015 by Uwe Trottmann."+
+					"         Icons made by  "+
+					"         <a href='http://www.icomoon.io' title='Icomoon'>Icomoon</a>, "+
+					"         <a href='http://catalinfertu.com' title='Catalin Fertu'>Catalin Fertu</a>, "+
+					"         <a href='http://linhpham.me/miu' title='Linh Pham'>Linh Pham</a> "+
+					"         from <a href='http://www.flaticon.com' title='Flaticon'>www.flaticon.com</a>  "+
+					"         is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0'>CC BY 3.0</a> "+
+					"       </p>  "+
+					"       <hr>"+
+					"         <div>"+
+					"           <div style'display:inline-block'>"+
+					"             <b>Powered by: <a href='http://themoviedb.org'>TMDB</a> and <a href='http://trakt.tv'>Trakt.tv</a></b>"+
+					"           </div>"+
+					"           <div style='float:left;display:inline-block'>"+
+					"               %s"+
+					"           </div>"+
+					"           <div style='display:inline-block'>"+
+					"               %s"+
+					"           </div>"+
+					"               <div style='font-size:80%%'>This product uses the TMDB API but is not endorsed or certified by TMDB.</div>"+
+					"         </div>"+
+					"       </div> "+
+					"         %s"+
+					"       </div>"+
+					"       <div>"+
+					"       </div>"+
+					"  </body> "+
+					" </html> ";
 
 
 	@Override
@@ -87,12 +84,14 @@ public class AboutActivity extends Activity {
 						   "</div>";
 		}
 		
-		webview.loadData(_prefix+getBitmapHtml(R.drawable.ic_launcher,new Integer(128),null)+
-				_middle+
-				getBitmapHtml(R.drawable.tmdb_logo,null,null)+_tmdb_postfix+
-				getBitmapHtml(R.drawable.trakt_logo,null,null)+_trakt_postfix+
-				debug+
-				_terminal_postfix,
+		webview.loadData(String.format(_html, 
+				getBitmapHtml(R.drawable.ic_launcher,new Integer(128),null),
+				Environment.NAME,
+				Environment.VERSION,
+				Environment.COPYRIGHT,
+				getBitmapHtml(R.drawable.tmdb_logo,null,null),
+				getBitmapHtml(R.drawable.trakt_logo,null,null),
+				debug),
 				"text/html; charset=utf-8;", "utf-8");
 		
         ActionBar actionBar = getActionBar();
