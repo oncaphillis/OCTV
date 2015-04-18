c8a09abdf48cd0b4c42d146fedf37761e48c45cf
Sebastian Kloska <oncaphillis@snafu.de>
2015-04-18 12:28:08 +0200

    #4 Generate a specialized "SeasonView" in EpisodePager

--- src/net/oncaphillis/whatsontv/EpisodeObjectFragment.java
+++ src/net/oncaphillis/whatsontv/EpisodeObjectFragment.java
@@ -21,21 +21,15 @@ import android.widget.TextView;
 
 public class EpisodeObjectFragment extends EntityInfoFragment {
 
-	private View _rootView = null;
-	private int _series;
-	private int _season;
-	private int _episode;
 	private TaskObserver _threadObserver = null; 
 
     private static final String _prefix = "<html>"+
 			   " <body style='background-color: #000000; color: #ffffff'>";
 
     private static final String _postfix = "</body></html>";
-
 	
 	public EpisodeObjectFragment() {
 	}
-
 
 	@Override
     public View onCreateView(LayoutInflater inflater,
@@ -43,20 +37,30 @@ public class EpisodeObjectFragment extends EntityInfoFragment {
 
 		Bundle args = getArguments();
 		
-		_series  = args.getInt("series");
-		_season  = args.getInt("season");
-		_episode = args.getInt("episode");
+		int series  = args.getInt("series");
+		int season  = args.getInt("season");
+		int episode = args.getInt("episode");
+		return episode != 0 ? createEpisodeView(inflater,container,series,season,episode) : 
+			createSeasonView(inflater,container,series,season);
+	}
+	
+	private View createSeasonView(LayoutInflater inflater, ViewGroup container, final int series,final int season) {
+		View theView    = inflater.inflate(R.layout.season_fragment, container, false);
+		return theView;
+	}
+	
+	private View createEpisodeView(LayoutInflater inflater, ViewGroup container, final int series,final int season,final int episode) {
 		
-		_rootView    = inflater.inflate(R.layout.episode_fragment, container, false);
+		View theView    = inflater.inflate(R.layout.episode_fragment, container, false);
 		
-		final WebView  overview_webview    = ((WebView) _rootView.findViewById(R.id.episode_fragment_overview));
-		final ImageView episode_still      = ((ImageView) _rootView.findViewById(R.id.episode_stillpath));
+		final WebView  overview_webview    = ((WebView)   theView.findViewById(R.id.episode_fragment_overview));
+		final ImageView episode_still      = ((ImageView) theView.findViewById(R.id.episode_stillpath));
 		
-        final TextView tv_header           = ((TextView) _rootView.findViewById(R.id.episode_fragment_title));
-        final TextView tv_rating           = ((TextView) _rootView.findViewById(R.id.episode_page_voting));
-        final TextView tv_rating_count     = ((TextView) _rootView.findViewById(R.id.episode_page_voting_count));
-        final TextView tv_date_tag         = ((TextView) _rootView.findViewById(R.id.episode_fragment_nearest_tag));
-        final TextView tv_date             = ((TextView) _rootView.findViewById(R.id.episode_fragment_last_aired));
+        final TextView tv_header           = ((TextView) theView.findViewById(R.id.episode_fragment_title));
+        final TextView tv_rating           = ((TextView) theView.findViewById(R.id.episode_page_voting));
+        final TextView tv_rating_count     = ((TextView) theView.findViewById(R.id.episode_page_voting_count));
+        final TextView tv_date_tag         = ((TextView) theView.findViewById(R.id.episode_fragment_nearest_tag));
+        final TextView tv_date             = ((TextView) theView.findViewById(R.id.episode_fragment_last_aired));
 
         final String no_overview = getActivity().getResources().getString(R.string.no_overview_available);
         final String aires = getActivity().getResources().getString(R.string.aires);
@@ -69,11 +73,11 @@ public class EpisodeObjectFragment extends EntityInfoFragment {
 			
 			@Override
 			public void run() {
-				final TvSeries series = Tmdb.get().loadSeries(_series);
-				final EpisodeInfo episode = Tmdb.get().loadEpisode(_series, _season, _episode);
+				final TvSeries tvs = Tmdb.get().loadSeries( series);
+				final EpisodeInfo tve = Tmdb.get().loadEpisode( series,  season,  episode);
 				final String ds;
 				
-				if(series!= null && episode!=null) {
+				if(tvs!= null && tve!=null) {
 
 					// If we currently do not have a clock value
 			        // for this episode we might want to get informed 
@@ -84,7 +88,7 @@ public class EpisodeObjectFragment extends EntityInfoFragment {
 						
 						@Override
 						public void run() {
-							final Date date = episode.getAirTime();
+							final Date date = tve.getAirTime();
 							if(fragment.getActivity()!=null) {
 								fragment.getActivity().runOnUiThread(new Runnable() {
 									@Override
@@ -108,10 +112,10 @@ public class EpisodeObjectFragment extends EntityInfoFragment {
 					tv_date.setTag(r0);
 					Tmdb.get().trakt_reader().register(r0);
 					
-					Date date  = episode.getAirTime() == null ? episode.getAirDate() : episode.getAirTime();
+					Date date  = tve.getAirTime() == null ? tve.getAirDate() : tve.getAirTime();
 					Date today = TimeTool.getToday();
 
-					episode_still.setTag(episode.getTmdb().getStillPath());
+					episode_still.setTag(tve.getTmdb().getStillPath());
 			        new BitmapDownloaderTask(episode_still, 3, getActivity(), null, null, null).execute();
 			        
 			        if(getActivity()!=null) {
@@ -119,28 +123,28 @@ public class EpisodeObjectFragment extends EntityInfoFragment {
 						final Activity act = getActivity();
 			        	getActivity().runOnUiThread(new Runnable() {
 
-							float  _voteAverage = episode.getTmdb().getVoteAverage();
-							int    _voteCount = episode.getTmdb().getVoteCount();
-							String _overview = episode.getTmdb().getOverview();
+							float  _voteAverage = tve.getTmdb().getVoteAverage();
+							int    _voteCount = tve.getTmdb().getVoteCount();
+							String _overview = tve.getTmdb().getOverview();
 							
-							boolean withTime = episode.getAirTime()==null ? false : true;
+							boolean withTime = tve.getAirTime()==null ? false : true;
 							
 							@Override
 							public void run() {
 								String episodes_for = act.getResources().getString(R.string.episodes_for);
-								act.setTitle(String.format(episodes_for,series.getName()));
+								act.setTitle(String.format(episodes_for,tvs.getName()));
 								
 								Date today = TimeTool.getToday();
 								
 								if(_overview == null || _overview == "")
 									_overview = no_overview;
 								
-								if(episode.getTmdb().getName() == null || episode.getTmdb().getName().equals(""))  {
+								if(tve.getTmdb().getName() == null || tve.getTmdb().getName().equals(""))  {
 									tv_header.setTextColor(act.getResources().getColor(R.color.oncaphillis_light_grey));
 									tv_header.setText(act.getResources().getString(R.string.no_title_available));
 								} else {
 									tv_header.setTextColor(act.getResources().getColor(R.color.oncaphillis_white));
-									tv_header.setText(episode.getTmdb().getName());
+									tv_header.setText(tve.getTmdb().getName());
 								}
 								
 								overview_webview.loadData(_prefix+
@@ -177,6 +181,6 @@ public class EpisodeObjectFragment extends EntityInfoFragment {
 		if(_threadObserver!=null)
 			_threadObserver.add(t);
 
-        return _rootView;
+        return theView;
     }
 }
