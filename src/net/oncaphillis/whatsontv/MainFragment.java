a502233886ed3f9179850c3e0bb554f61509308a
Sebastian Kloska <oncaphillis@snafu.de>
2015-04-26 10:43:24 +0200

    #40 Save and restore latest top position when rotating

--- src/net/oncaphillis/whatsontv/MainFragment.java
+++ src/net/oncaphillis/whatsontv/MainFragment.java
@@ -39,12 +39,14 @@ public class MainFragment extends Fragment {
     		ViewGroup container, Bundle savedInstanceState) {
 		Bundle b = this.getArguments();
 		
+		final int pos = savedInstanceState!=null  && savedInstanceState.containsKey("top") ? savedInstanceState.getInt("top") : 0;
+		
 		_idx  = b.getInt("idx");
 		
 		int cols = Environment.getColumns(_activity);
 
 		_theView   = inflater.inflate(R.layout.main_fragment, container, false);
-				
+						
 		_progressBar  = (ProgressBar)_theView.findViewById(R.id.load_progress);
 		
 		
@@ -52,93 +54,106 @@ public class MainFragment extends Fragment {
 		
 		_mainGridView.setNumColumns(cols);
 		
-			Object o = Environment.ListAdapters[_idx];
-				
-			_mainGridView.setAdapter(Environment.ListAdapters[_idx]);
+		Object o = Environment.ListAdapters[_idx];
 			
-			_mainGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-					Intent myIntent = new Intent(_activity, SeriesPagerActivity.class);
-					Bundle b        = new Bundle();
-					synchronized(Environment.ListAdapters[_idx]) {
-						int[]    ids   = new int[Environment.ListAdapters[_idx].getCount()];
-						String[] names = new String[Environment.ListAdapters[_idx].getCount()];
-						
-						for(int ix=0;ix<Environment.ListAdapters[_idx].getCount();ix++) {
-							ids[ix]   = Environment.ListAdapters[_idx].getItem(ix).getId();
-							names[ix] = Environment.ListAdapters[_idx].getItem(ix).getName();
-						}
-						
-						b.putString(SeriesObjectFragment.ARG_TITLE, Environment.Titles[_idx]);
-						b.putIntArray("ids", ids);
-						b.putStringArray("names", names);
-						b.putInt("ix", position);
+		_mainGridView.setAdapter(Environment.ListAdapters[_idx]);
+		
+		_mainGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+				Intent myIntent = new Intent(_activity, SeriesPagerActivity.class);
+				Bundle b        = new Bundle();
+				synchronized(Environment.ListAdapters[_idx]) {
+					int[]    ids   = new int[Environment.ListAdapters[_idx].getCount()];
+					String[] names = new String[Environment.ListAdapters[_idx].getCount()];
 					
-						myIntent.putExtras(b);
-						startActivity(myIntent);
+					for(int ix=0;ix<Environment.ListAdapters[_idx].getCount();ix++) {
+						ids[ix]   = Environment.ListAdapters[_idx].getItem(ix).getId();
+						names[ix] = Environment.ListAdapters[_idx].getItem(ix).getName();
 					}
-				}
-			});
-			
-			if(_activity!=null && _activity instanceof MainActivity) {
-				
-				final MainActivity act = (MainActivity)_activity;
-				final MainFragment f = this;
-				final LinearLayout ll = (LinearLayout)_theView.findViewById(R.id.no_list_found_layout);
-				
-				ll.setVisibility(View.GONE);
-				
-				new Thread(new Runnable() {
 					
-					@Override
-					public void run() {
-						while(act!=null && act.SearchThread!=null && act.SearchThread.getState()!=Thread.State.TERMINATED) {
-							act.runOnUiThread(new Runnable(){
-								@Override
-								public void run() {
-									Current ci = act.SearchThread.getCurrent();
-									synchronized(act) {
-										if( f.getIdx() < ci.list) {
-											f.setProgressBarVisibility(false);							
-											if(act.SearchThread.getCount(f.getIdx()) == 0)
-												ll.setVisibility(View.VISIBLE);
-										} else if(f.getIdx()==ci.list && ci.count>0) {
-											f.setProgressBarVisibility(true);
-											f.setProgressBarIndeterminate(false);
-											f.setProgress(ci.count * 10000 / ci.total);
-										} else {
-											f.setProgressBarVisibility(true);
-											f.setProgressBarIndeterminate(true);
-										}
-									}
-								}
-							});
-							
-							try {
-								Thread.sleep(1000);
-							} catch (InterruptedException e) {
-							}
-						}
-						
+					b.putString(SeriesObjectFragment.ARG_TITLE, Environment.Titles[_idx]);
+					b.putIntArray("ids", ids);
+					b.putStringArray("names", names);
+					b.putInt("ix", position);
+				
+					myIntent.putExtras(b);
+					startActivity(myIntent);
+				}
+			}
+		});
+			
+		if(_activity!=null && _activity instanceof MainActivity) {
+			
+			final MainActivity act = (MainActivity)_activity;
+			final MainFragment f = this;
+			final LinearLayout ll = (LinearLayout)_theView.findViewById(R.id.no_list_found_layout);
+			
+			ll.setVisibility(View.GONE);
+			
+			new Thread(new Runnable() {				
+				int npos = pos;
+				@Override
+
+				public void run() {
+					while(act!=null && act.SearchThread!=null && act.SearchThread.isAlive()) {
 						act.runOnUiThread(new Runnable(){
 							@Override
 							public void run() {
-								if(act.SearchThread.getCount(f.getIdx()) == 0)
-									ll.setVisibility(View.VISIBLE);
-								f.setProgressBarIndeterminate(false);
-								f.setProgressBarVisibility(false);
+								Current ci = act.SearchThread.getCurrent();
+								synchronized(act) {
+									if( f.getIdx() < ci.list) {
+										f.setProgressBarVisibility(false);							
+										
+										if(npos!=-1) {
+											_theView.requestFocusFromTouch();
+											_mainGridView.setSelection(npos);
+											npos=-1;
+										}
+											
+										if(act.SearchThread.getCount(f.getIdx()) == 0)
+											ll.setVisibility(View.VISIBLE);
+										
+									} else if(f.getIdx()==ci.list && ci.count>0) {
+										f.setProgressBarVisibility(true);
+										f.setProgressBarIndeterminate(false);
+										f.setProgress(ci.count * 10000 / ci.total);
+									} else {
+										f.setProgressBarVisibility(true);
+										f.setProgressBarIndeterminate(true);
+									}
+								}
 							}
 						});
+						
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+						}
 					}
-				}).start();
-			}
-			
+					
+					act.runOnUiThread(new Runnable(){
+						@Override
+						public void run() {
+							if(act.SearchThread.getCount(f.getIdx()) == 0)
+								ll.setVisibility(View.VISIBLE);
+							_theView.requestFocusFromTouch();
+							if(npos!=-1)
+								_mainGridView.setSelection(npos);
+							f.setProgressBarIndeterminate(false);
+							f.setProgressBarVisibility(false);
+						}
+					});
+				}
+			}).start();
+		}
         return _theView;
     }
 		
 	@Override
 	public void onSaveInstanceState(Bundle outState) { 
+		if(_mainGridView!=null)
+			outState.putInt("top", _mainGridView.getFirstVisiblePosition());
 		super.onSaveInstanceState(outState);
 	}
 	
