4a9500ff3e06eb3f2d74e6591177356590d39301
Sebastian Kloska <oncaphillis@snafu.de>
2015-04-28 01:18:22 +0200

    #14 New Tables and Indices for Data Caching

--- src/net/oncaphillis/whatsontv/SQLCacheHelper.java
+++ src/net/oncaphillis/whatsontv/SQLCacheHelper.java
@@ -6,9 +6,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class SQLCacheHelper extends SQLiteOpenHelper {
 
-    private static final int    DATABASE_VERSION = 2;
+    private static final int    DATABASE_VERSION  = 5;
     private static final String SERIES_TABLE_NAME = "SERIES";
     private static final String SEASON_TABLE_NAME = "SEASON";
+    private static final String EPISODE_TABLE_NAME = "EPISODE";
     private static final String DATABASE_NAME = Environment.NAME;
     		
     private static final String SERIES_TABLE_CREATE =
@@ -16,13 +17,35 @@ public class SQLCacheHelper extends SQLiteOpenHelper {
             " (ID INTEGER PRIMARY KEY ASC,TIMESTAMP INTEGER NOT NULL,DATA BLOB NOT NULL);";
 
     private static final String SEASON_TABLE_CREATE =
-            "CREATE TABLE SEASON "+
+            "CREATE TABLE " + SEASON_TABLE_NAME +
             " (SERIES    INT  NOT NULL,"+
             "  ID        INT  NOT NULL,"+
             "  TIMESTAMP INT  NOT NULL,"+
             "  DATA      BLOB NOT NULL,"+
             "  PRIMARY KEY(SERIES,ID) ,"+
             "  FOREIGN KEY(SERIES) REFERENCES SERIES(ID) ON DELETE CASCADE);";
+
+    private static final String EPISODE_TABLE_CREATE =
+            "CREATE TABLE " + EPISODE_TABLE_NAME +
+            " (SERIES    INT  NOT NULL,"+
+            "  SEASON    INT  NOT NULL,"+
+            "  ID        INT  NOT NULL,"+
+            "  TIMESTAMP INT  NOT NULL,"+
+            "  DATA      BLOB NOT NULL,"+
+            "  PRIMARY KEY(SERIES,SEASON,ID) ,"+
+            "  FOREIGN KEY(SERIES,SEASON) REFERENCES SEASON(SERIES,ID) ON DELETE CASCADE);";
+
+    private static final String SERIES_INDEX_CREATE =
+            "CREATE INDEX " + SERIES_TABLE_NAME + "_IDX1 " + 
+            " ON " + SERIES_TABLE_NAME+"(TIMESTAMP);";
+
+    private static final String SEASON_INDEX_CREATE =
+            "CREATE INDEX " + SEASON_TABLE_NAME + "_IDX1 " + 
+            " ON " + SEASON_TABLE_NAME+"(TIMESTAMP);";
+
+    private static final String EPISODE_INDEX_CREATE =
+            "CREATE INDEX " + EPISODE_TABLE_NAME + "_IDX1 " + 
+            " ON " + SEASON_TABLE_NAME+"(TIMESTAMP);";
 
     SQLCacheHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -32,12 +55,34 @@ public class SQLCacheHelper extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
         db.execSQL(SERIES_TABLE_CREATE);
         db.execSQL(SEASON_TABLE_CREATE);
+        db.execSQL(EPISODE_TABLE_CREATE);
+
+        db.execSQL(SERIES_INDEX_CREATE);
+        db.execSQL(SEASON_INDEX_CREATE);
+        db.execSQL(EPISODE_INDEX_CREATE);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		if(newVersion>1)
+		
+		if(newVersion<=1)
 			db.execSQL(SEASON_TABLE_CREATE);
+		
+		if(oldVersion<=2) {
+			db.execSQL(SERIES_INDEX_CREATE);
+			db.execSQL(SEASON_INDEX_CREATE);
+		}
+		if(oldVersion<=3) {
+			db.execSQL(EPISODE_TABLE_CREATE);
+			db.execSQL(EPISODE_INDEX_CREATE);
+		}		
+		if(oldVersion<=4) {
+			db.execSQL("drop index "+EPISODE_TABLE_NAME+"_IDX1");
+			db.execSQL("drop table "+EPISODE_TABLE_NAME);
+			db.execSQL(EPISODE_TABLE_CREATE);
+			db.execSQL(EPISODE_INDEX_CREATE);
+		}		
+		return;
 	}
 	
 	@Override
